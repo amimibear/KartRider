caf89c0775b9d9032ba55bc54ee233ef4307a45a
amimibear <amimibear@gmail.com>
2023-02-27 21:35:16 +0100

    gitignore

--- ZhenCangGuan/main.py
+++ ZhenCangGuan/main.py
@@ -34,23 +34,37 @@ import csv
 
 
 
-dp, tp =set(), set()
+dp, tp = {}, {}
+double = {'多彩气球炫光':[44,45],'汽车之家气球':[39,42],'黑妞生日气球':[36,43],'跑跑新生服饰':[27,34],'跑跑新生发型':[26,27]}
+dp2 =  {'多彩气球炫光[1]':44,
+        '多彩气球炫光[2]':45,
+        '汽车之家气球[紫]': 39,
+        '汽车之家气球[黄]': 42,
+        '黑妞生日气球[皇冠]': 36,
+        '黑妞生日气球[帽子]': 43,
+        '跑跑新生服饰[1]': 27,
+        '跑跑新生服饰[2]': 34,
+        '跑跑新生发型[1]': 26,
+        '跑跑新生发型[2]': 27,
+        } # 这些东西竟然有两个value
 with open("dp.csv", newline="") as f:
     reader = csv.reader(f)
     for row in reader:
-        dp.add((row[0],int(row[1])))
+        if row[0] in dp:
+            print('wait!!!!!!!!!!!')
+        dp[row[0]]=int(row[1])
 with open("tp.csv", newline="") as f:
     reader = csv.reader(f)
     for row in reader:
-        tp.add((row[0],int(row[1])))
+        tp[row[0]]=int(row[1])
 
-ff = open('screen.txt', 'w')
+ff = open('data3.txt', 'w')
 
 for id in range(1000000):
-    print(id)
+    print(id,end=' ')
     im = ImageGrab.grab(bbox=(400, 130, 1340, 1160))  # X1,Y1,X2,Y2
     # # time.sleep(5)
-    im.save(f"img2/{id}.png")
+    im.save(f"img3/{id}.png")
 
     reader = easyocr.Reader(['ch_sim','en']) 
     # reader = easyocr.Reader(['ch_sim']) 
@@ -66,7 +80,6 @@ for id in range(1000000):
         if s[:3]=='藏珍馆':
             o = 1
         if o:
-            
             if ':' not in s and '+' not in s and '[' not in s:
                 o = 0
                 ll.append(''.join(l))
@@ -78,7 +91,7 @@ for id in range(1000000):
         ll.append(''.join(l))
     
     for s in ll:
-        print(s)
+        # print(s)
         ff.write(str(id)+' '+s+'\n')
         while '[' in s or ']' in s:
             i = 0
@@ -125,13 +138,29 @@ for id in range(1000000):
                 s = s[:i]+('O' if s[i+1].isupper() else 'o')+s[i+1:]
         for i in range(len(s)-4):
             if s[i:i+4]=='1000' or s[i:i+4]=='2023' or s[i:i+4]=='2022':
-                s = s[:i]+' '+s[:i+4]
-        print(s)
+                s = s[:i]+' '+s[i+4:]
+        
+        m = {1:{'〈':'(','〉':')','孑':'子','芾':'带','麇':'麋','圭':'主','曰':'日','居':'尾','爰':'爱'},
+             2:{'字航':'宇航','-夏':'一夏','2翼':'之翼','2星':'之星','7星':'之星','自色':'白色','滨o':'滨DJ','揭蛋':'捣蛋','萌鬼':'萌兔','告自':'告白','人7':'人Z','黑自':'黑白','末来':'未来','=角':'三角',
+                '酉几':'西几','月镜':'目镜','-击':'一击','士著':'土著','@P':'CP','挑-':'挑一','自羊':'白羊','塞冰':'寒冰','战土':'战士','尺军':'R军'}}
+        for size in range(1,3):
+            for i in range(len(s)-size+1):
+                w = s[i:i+size]
+                if w in m[size]:
+                    s = s[:i]+m[size][w]+s[i+size:]
+        for i in range(3,len(s)-1): # 处理 S**车王/神炫光 中S识别成5的问题
+            if s[i:i+2]=='车王' or s[i:i+2]=='车神':
+                if s[i-2]=='5':
+                    s = s[:i-2]+' S'+s[i-1:]
+                elif s[i-3]=='5':
+                    s = s[:i-3]+' S'+s[i-2:]
+        
+        # print(s)
         ff.write(str(id)+' '+s+'\n')
 
         # l = [w for w in sum([re.split("[:+/. ]", ''.join(j)) for i,j in groupby(re.sub(u'\\[.*?\\]','',s), key=lambda x: x.isdigit())],[]) if w]
-        l = [w for w in sum([re.split("[':+/. ]", ''.join(j)) for i,j in groupby(s, key=lambda x: x.isdigit())],[]) if w]
-        print(l)
+        l = [w for w in sum([re.split("[';:+/., ]|`", ''.join(j)) for i,j in groupby(s, key=lambda x: x.isdigit() or x=='S')],[]) if w]
+        # print(l)
         o = 0 # 1 单品 2 套品
         t = []
         for w in l:
@@ -147,44 +176,48 @@ for id in range(1000000):
             elif w[:2]=='套品':
                 o = 2
                 w = w[2:]
-            if o==1:
+            if o==1: # 单品
                 if w.isdigit():
+                    # if len(t)!=1:
+                    #     print(id,'too long ',ss,' ! ',s)
                     if int(w)>200 and w[-1]=='1':
                         w = w[:-1]
+                    w = int(w)
                     ss = ' '.join(t)
-                    if len(t)!=1:
-                        print(id,'too long ',ss,' ! ',s)
-                    dp.add((ss,int(w)))
+                    sp = '+'.join(t)
+                    ls = t[-1]
                     t = []
+                    if ls in dp and dp[ls]==w: # 最后一位有一样的不要了 防止前面有乱七八糟的东西
+                        continue
+                    if sp in tp and tp[sp]==w: # 套品有一样的也不要了
+                        continue
+                    if ss in dp and dp[ss]!=w: # 处理同物品不同value
+                        if w<20 or w>200 or w%100==dp[ss]: # 可能前面多个1
+                            continue
+                        else:
+                            ff.write(f'\n!!! {w} {id} {s}\n\n')
+                    dp[ss] = w
                 else:
                     t.append(w)
-            elif o==2:
+            elif o==2: # 套品
                 if w.isdigit():
-                    if len(t)<=1:
-                        print(id,'too short ',w,' ! ',s)
+                    # if len(t)<=1:
+                    #     print(id,'too short ',w,' ! ',s)
+                    w = int(w)
                     ss = '+'.join(t)
-                    tp.add((ss,int(w)))
                     t = []
+                    if ss in tp and tp[ss]!=w: # 处理同物品不同value
+                        if w<20 or w>200 or w%100==dp[ss]: # 可能前面多个1
+                            continue
+                        else:
+                            ff.write(f'\n!!! {w} {id} {s}\n\n')
+                    tp[ss] = w            
                 else:
                     t.append(w)
     # print(dp,'\n',tp,'\n')
     with open("dp.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(sorted(list(dp),key=lambda x:(-x[1],x[0])))
+        writer.writerows(sorted(list(dp.items())+list(dp2.items()),key=lambda x:(-x[1],x[0])))
     with open("tp.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(sorted(list(tp),key=lambda x:(-x[1],x[0])))
-
-
-
-
-                    
-
-
-            
-
-
-
-
-    # # 结果
-    # print(result)
+        writer.writerows(sorted(list(tp.items()),key=lambda x:(-x[1],x[0])))
--- ZhenCangGuan/sort.py
+++ ZhenCangGuan/sort.py
@@ -7,25 +7,50 @@ import re
 import csv
 
 
-dp, tp =set(), set()
-with open("dp1.1.csv", newline="") as f:
+dp, tp, dp2 = {}, {}, {}
+
+# dp2 =  {'多彩气球炫光[1]':44,
+#         '多彩气球炫光[2]':45,
+#         '汽车之家气球[紫]': 39,
+#         '汽车之家气球[黄]': 42,
+#         '黑妞生日气球[皇冠]': 36,
+#         '黑妞生日气球[帽子]': 43,
+#         '跑跑新生服饰[1]': 27,
+#         '跑跑新生服饰[2]': 34,
+#         '跑跑新生发型[1]': 26,
+#         '跑跑新生发型[2]': 27,
+#         } # 这些东西竟然有两个value
+
+# dp
+
+with open("dp.csv", newline="") as f:
     reader = csv.reader(f)
     for row in reader:
-        dp.add((row[0],int(row[1])))
+        if row[0] in dp:
+            dp2[row[0]]=int(row[1])
+            print(row)
+            continue
+        dp[row[0]]=int(row[1])
+
+print(dp2)
+
+with open("dp0.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(sorted(list(dp.items())+list(dp2.items()),key=lambda x:(-x[1],x[0])))
+
+
+# tp
+
 # with open("tp.csv", newline="") as f:
 #     reader = csv.reader(f)
 #     for row in reader:
-#         tp.add((row[0],int(row[1])))
-
-
-with open("dp1.1.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerows(sorted(list(dp),key=lambda x:(-x[1],x[0])))
-# with open("tpa.csv", "w", newline="") as f:
+#         if row[0] in tp:
+#             print(row)
+#             continue
+#         tp[row[0]]=int(row[1])
+# with open("tp.csv", "w", newline="") as f:
 #     writer = csv.writer(f)
-#     writer.writerows(sorted(list(tp),key=lambda x:x[0]))
-
-
+#     writer.writerows(sorted(list(tp.items()),key=lambda x:(-x[1],x[0])))
 
 
                     
