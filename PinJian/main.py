44ebde8b82e1251a1ab8445375c390a4cad8f34c
amimibear <amimibear@gmail.com>
2023-03-03 23:40:41 +0100

    1933

--- PinJian/main.py
+++ PinJian/main.py
@@ -6,7 +6,7 @@ from itertools import groupby
 import re
 import csv
 import os
-
+from paddleocr import PaddleOCR
 
 
 # time.sleep(10)
@@ -45,26 +45,41 @@ with open("taopin.csv", newline="") as f:
     for row in reader:
         tp[row[0]]=int(row[1])
 
-N = 12
 
-folder = f'img/img{N}'
-if not os.path.exists(folder):
-    os.makedirs(folder)
+N = 15
 
+ocr = PaddleOCR(use_angle_cls=False, lang='ch') # need to run only once to download and load model into memory # ocr_version='PP-OCRv2'
+
+# folder = f'img/img{N}'
+# if not os.path.exists(folder):
+#     os.makedirs(folder)
+id0 = int(open(f'img/img/_ok.txt', 'r').read())+1
+id0 = 1618
 ff = open(f'data/data{N}.txt', 'w')
 reader = easyocr.Reader(['ch_sim','en']) 
-for id in range(1000000):
+for id in range(id0,1000000):
 # for id in range(1,6):
     print(id)
     # im = ImageGrab.grab(bbox=(400, 130, 1340, 1160))  # X1,Y1,X2,Y2
-    im = ImageGrab.grab(bbox=(-1240, 150, -700, 755))  # X1,Y1,X2,Y2
+    # im = ImageGrab.grab(bbox=(-1240, 150, -700, 755))  # X1,Y1,X2,Y2
+    # im = ImageGrab.grab(bbox=(-1200, 30, -610, 870))  # X1,Y1,X2,Y2 ipad
     # # time.sleep(5)
     # im.save(f"img/img{N}/{id}.png")
+    save = 0 
+    result = ocr.ocr(f'img/img/{id}.png', cls=False) # PaddleOCR快，用来过滤
+    for idx in range(len(result)):
+        res = result[idx]
+        for line in res:
+            if '藏' in line[1][0]:
+                save = 1
+    if not save:
+        os.remove(f"img/img/{id}.png")
+        open('img/img/_ok.txt', 'w').write(str(id))
+        continue
 
-    # reader = easyocr.Reader(['ch_sim']) 
-    # # 读取图像
-    # result = reader.readtext(f'img/img1/{id}.png',detail=0)
-    result = reader.readtext(np.array(im),detail=0)
+    save = 0
+    result = reader.readtext(f'img/img/{id}.png',detail=0)
+    # result = reader.readtext(np.array(im),detail=0)
 
     o = 0
     l = []
@@ -84,9 +99,7 @@ for id in range(1000000):
     if o:
         ll.append(''.join(l))
     
-    if ll:
-        im.save(f"img/img{N}/{id}.png")
-    
+
     for s in ll:
         print(s)
         ff.write(str(id)+' '+s+'\n')
@@ -138,16 +151,17 @@ for id in range(1000000):
                 s = s[:i]+' '+s[i+4:]
         
 
-        dd = ['颜值','L','动萌兔花束','心动萌兔套装女发型','子背带裤(男)','联赛无畏服(女)','WKC 气球','WKC 趣味眼镜','C助威头盔(女)','KLC助威头盔(女)','KLC助威头盔(男)','LC助威头盔(男)','C助威头盔(男)','二项循环炫光','月气球','青蛙头饰','FormulaC气球','FormulaE气球','瑗逮面饰','强红色喷漆','边太阳镜','博士紫色喷漆','浪漫枫情帽男)','-叶知秋眼镜','|雨伞头饰','祥云气球','万里挑气球','小小悟空的金箍棒','心相印发箍','音乐气球',
+        dd = ['颜值','L','相约','动萌兔花束','心动萌兔套装女发型','子背带裤(男)','联赛无畏服(女)','WKC 气球','WKC 趣味眼镜','C助威头盔(女)','KLC助威头盔(女)','KLC助威头盔(男)','LC助威头盔(男)','C助威头盔(男)','二项循环炫光','月气球','青蛙头饰','FormulaC气球','FormulaE气球','瑗逮面饰','强红色喷漆','边太阳镜','博士紫色喷漆','浪漫枫情帽男)','-叶知秋眼镜','|雨伞头饰','祥云气球','万里挑气球','小小悟空的金箍棒','心相印发箍','音乐气球',
               '心动萌兔套装女服装','WkC头盔','凤气球','虎气球','娃黄色喷漆','一娃黄色喷漆','C娃黄色喷漆','^娃橙色喷漆','KC助威战服(女)','动萌兔气球','驻场D服装','氮气少少女装','KC助威头盔(男)','Formula C 气球','燃擎夏装(女)','鹿气球','小小恐龙气球','燃擎一夏发型男)','Ump气球','|F闺蜜眼镜','燃擎夏皇冠','瑞舞狮气球','船修理包','小生日帽',
-              '二娃黄色喷漆','一娃橙色喷漆','天炫光','个气球','-朵小花','KLC炫光','幻彩帽((男)']
+              '二娃黄色喷漆','一娃橙色喷漆','天炫光','个气球','-朵小花','朵小花','KLC炫光','幻彩帽((男)','Wkc头盔','C助威战服(女)','爱手杖','叶知秋眼镜','蜜蜂气球','天使之翼','斯特气球']
         dt = ['心动萌兔套装-男发型+心动萌兔套装男服装+心动萌兔面饰+心动萌兔花束','心动萌兔套装男发型+心动萌兔套装-男服装+心动萌兔面饰+心动萌兔花束','福虎迎春帽(男)+福虎迎春服(男+福禄寿背饰','小小青蛙头饰+新手鱼竿+蛙蛙荷塘炫光','燃擎一夏头盔+燃擎一夏发型(男)+燃擎夏装(男)','燃擎夏头盔+燃擎一夏发型(男)+燃擎一夏装(男)','心动萌兔套装-女发型+心动萌兔套装女服装','心动萌兔套装女发型+心动萌兔套装-女服装','鸳鸯软帽+(男)+蓝色衷心罩衫','C助威头盔(女)+KLC助威战服(女)','C助威战服(女)','KLC助威头盔(女)+KLC助威战服(女)',
-              '联赛无畏服(女)']
+              '联赛无畏服(女)','字芒星面饰+典礼之星手杖','助威战服(女)']
         
         m = {1:{'〈':'(','〉':')','孑':'子','芾':'带','麇':'麋','圭':'主','曰':'日','居':'尾','爰':'爱','肘':'时','魃':'魅','酉':'西','免':'兔','泠':'冷','=':'三'},
              2:{'字航':'宇航','-夏':'一夏','2翼':'之翼','自色':'白色','滨o':'滨DJ','场o':'场DJ','揭蛋':'捣蛋','萌鬼':'萌兔','告自':'告白','人7':'人Z','黑自':'黑白','末来':'未来','二角':'三角',
                 '月镜':'目镜','-击':'一击','士著':'土著','@P':'CP','挑-':'挑一','自羊':'白羊','塞冰':'寒冰','战土':'战士','尺军':'R军','捐挥':'指挥','OJ':'DJ','萸雄':'英雄','精萸':'精英','萸灵':'英灵','自云':'白云','护日':'护目',
-                '@博':'Q博','考虎':'老虎','显景':'显影','浪浸':'浪漫','自天':'白天','自虎':'白虎','-足':'十足','闺蝥':'闺蜜','自槿':'白槿','紧色':'紫色','粉@':'粉色','手耷':'手套','丰播':'主播'},
+                '@博':'Q博','考虎':'老虎','显景':'显影','浪浸':'浪漫','自天':'白天','自虎':'白虎','-足':'十足','闺蝥':'闺蜜','自槿':'白槿','紧色':'紫色','粉@':'粉色','手耷':'手套','丰播':'主播','宇护':'守护','花柬':'花束','法者':'法老','法茗':'法老',
+                '垂鬈':'垂髫','茗板':'老板','壬子':'王子','萝|':'萝卜','神赝':'神鹰'},
              3:{'未未来':'未来'}}
         for size in range(1,4):
             for i in range(len(s)-size+1):
@@ -195,6 +209,8 @@ for id in range(1000000):
                     ss = ''.join(t)
                     ss = ss.replace('^','')
                     sp = '+'.join(t)
+                    if sp[0]=='|':
+                        sp = sp[1:]
                     if sp[0]=='品':
                         sp = sp[1:]
                         if sp[0]==' ':
@@ -207,15 +223,23 @@ for id in range(1000000):
                         continue
                     if sp in tp and tp[sp]==w: # 套品有一样的也不要了
                         continue
+                    if ss in dp and dp[ss]==w: # 重复
+                        continue
                     if ss in dp and dp[ss]!=w: # 处理同物品不同value
                         if not (w<20 or w>200 or w%100==dp[ss]): # 可能前面多个1
                             ff.write(f'\n!!! {id} {ss} {w} {s}\n\n')
                         continue
                     dp[ss] = w
+                    save = 1
                 else:
                     t.append(w)
             elif o==2: # 套品
                 if w.isdigit():
+                    if not t: # 防止 t[-1] 报错
+                        ff.write(f'\n??? {w} {id} {s}\n')
+                        continue
+                    if t[-1]=='l':
+                        t.pop()
                     # if len(t)<=1:
                     #     print(id,'too short ',w,' ! ',s)
                     w = int(w)
@@ -224,13 +248,20 @@ for id in range(1000000):
                     t = []
                     if ss in dt: # 处理常见识别错误
                         continue
+                    if ss in tp and tp[ss]==w: # 重复
+                        continue
                     if ss in tp and tp[ss]!=w: # 处理同物品不同value
                         if not (w<20 or w>200 or w%100==tp[ss]): # 可能前面多个1
                             ff.write(f'\n!!! {id} {ss} {w} {s}\n\n')
                         continue
-                    tp[ss] = w            
+                    tp[ss] = w
+                    save = 1        
                 else:
                     t.append(w)
+    if not save:
+        os.remove(f"img/img/{id}.png")
+    open('img/img/_ok.txt', 'w').write(str(id))
+
     # print(dp,'\n',tp,'\n')
     with open("danpin.csv", "w", newline="") as f:
         writer = csv.writer(f)
