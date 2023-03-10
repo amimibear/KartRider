0f0caeeeaf4ea7b1c69c85046d4518e1934cb97a
amimibear <amimibear@gmail.com>
2023-03-10 08:13:53 +0100

    tp171

--- PinJian/save_img.py
+++ PinJian/save_img.py
@@ -5,10 +5,12 @@ id0 = int(open(f'img/_.txt', 'r').read())+1
 
 for id in range(id0,id0+1300000):
     print(id)
-    # im = ImageGrab.grab(bbox=(400, 130, 1340, 1160))  # X1,Y1,X2,Y2
+    # im = ImageGrab.grab(bbox=(400, 130, 1340, 1160))  # X1,Y1,X2,Y2 iphone big
     # im = ImageGrab.grab(bbox=(-1240, 150, -700, 755))  # X1,Y1,X2,Y2 iphone
     im = ImageGrab.grab(bbox=(-1200, 30, -610, 870))  # X1,Y1,X2,Y2 ipad
-    # im = ImageGrab.grab(bbox=(270, 30, 880, 870))  # X1,Y1,X2,Y2 ipad
+    # im = ImageGrab.grab(bbox=(270, 30, 880, 870))  # X1,Y1,X2,Y2 ipad small
+    # im = ImageGrab.grab(bbox=(230, 150, 770, 760))  # X1,Y1,X2,Y2 iphone small
+
     time.sleep(3)
     im.save(f"img/{id}.png")
     open('img/_.txt', 'w').write(str(id))
