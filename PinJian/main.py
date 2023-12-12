dfba991b901b2b75d7be731b8963deb2f3002a0b
amimibear <amimibear@gmail.com>
2023-12-12 23:24:31 +0800

    、

--- PinJian/main.py
+++ PinJian/main.py
@@ -164,17 +164,6 @@ ff = open(f'data/data{N}.txt', 'w')
 id0 = int(open(f'img/_ok.txt', 'r').read())+1
 
 # id0 = 47101
-
-# 556ab6 蓝 25~47
-# c949e0 紫 51～80
-# ffe96c 黄 81~108
-# da664f 红 121～121
-
-cls = ['556ab6','556a66'
-       'c949e0','o949e0',
-       'ffe96c','tfe96c',
-       'da664f']
-
 # reader = easyocr.Reader(['ch_sim','en']) 
 # for id in range(id0,id0+1000):
 ocr1 = PaddleOCR() # need to run only once to download and load model into memory # ocr_version='PP-OCRv2'
@@ -194,11 +183,11 @@ f = [lambda x:ocr1.ocr(x, cls=False),
     #  lambda x:baidu(x,'general_basic'), # 标准版 改这边要改k>=len(f)-1 or 2
      ]
 
-test = 0
+test = 1
 
 if test:
     id0 -= 100000
-    id0 = 341287
+    id0 = 5019883
 for id in range(id0,10000000):
 # for id in range(4671788,4671789):
     if not test:
@@ -228,17 +217,17 @@ for id in range(id0,10000000):
                 print('no img!')
                 print('no img!',file=ff)
         image = Image.open(f'img/{id}.png')
-        if image.size != (850,950):
-            crop_area = (400, 0, 1250, 950) # 定义裁剪区域 (x1, y1, x2, y2)
+        if image.size != (650,950): # from S28
+            crop_area = (400, 0, 1050, 950) # 定义裁剪区域 (x1, y1, x2, y2)
             image = image.crop(crop_area)
         RGB = np.array(image.convert('RGB'))
         # if not test:
         #     os.remove(f"img/{id}.png") # 如果有新增，最后再保存回去 中途停止会丢失数据
-        if not np.any(np.all(RGB == [237, 163, 163], axis=-1)):
+        if not np.any(np.all(RGB == [227, 104, 80], axis=-1)):
             if not test:
                 os.remove(f"img/{id}.png")
-            print('no pink!')
-            print('no pink!',file=ff)
+            print('no red!')
+            print('no red!',file=ff)
             continue
     
         
@@ -253,88 +242,64 @@ for id in range(id0,10000000):
         continue
     if test:
         print(f'\n{id}')
+    
+    # 看最后一列像素 以是不是蓝色[251,253]分块 找出有红色(227)的块
     def find_blocks_with_conditions(arr, b_range=(251, 253)):
         b_in_range = np.logical_and(arr[:, -1, 2] >= b_range[0], arr[:, -1, 2] <= b_range[1])
         blocks = np.split(arr, np.where(np.diff(b_in_range) == 1)[0] + 1)
-        valid_blocks = [block for block in blocks if np.any(block[:, -1, 2]==163)]
+        valid_blocks = [block for block in blocks if np.any(block[:, -1, 2]==80)]
         return valid_blocks
     
     add = 0
     byy = 0 #是否有不一样的
     bd = 0
-    for k in range(len(f)):
-        print('ocr',k)
-        print('ocr',k,file=ff)
-        brk = 0
-        ll = [[],[]]
-        valid_blocks = find_blocks_with_conditions(RGB)
-        for block in valid_blocks:
-            result = f[k](block)
-            try:
+
+    valid_blocks = find_blocks_with_conditions(RGB)
+    for block in valid_blocks:
+        lines = np.split(block, np.where(np.diff(block[:,-1,0]==255) == 1)[0] + 1)
+        for i in range(len(lines)):
+            if len(lines[i])>120: # 超长的分两半 遇到套品了
+                lines = lines[:i] + [lines[i][:50],lines[i][50:]] + lines[i+1:]
+        first = f[0](lines[0]) # 冰雪之歌/夜行骑士
+        lines = lines[1:]
+        Type = ''.join([line[1][0] for res in first for line in res])
+        if "冰雪之歌" in Type:
+            n = 0
+        elif "夜行骑士" in Type:
+            n = 1
+        else:
+            raise Exception('not 冰雪之歌 or 夜行骑士!')
+        
+        dp = 1 # 是否是单品
+        for line in lines:
+            if line.shape[0]<40:
+                continue
+            num = int(f[0](line[:,500:])[0][0][1][0])
+            for k in range(len(f)):
+                print('ocr',k)
+                print('ocr',k,file=ff)
+
+                brk = 0
+                name = f[k](line[:,:500])
+                # try:
                 if k>=len(f)-1: # 最后一/两个是baidu
                     bd = 1 # 用baidu就save
-                    s = ''.join([line['words'] for line in result['words_result']])
+                    name = ''.join([l['words'] for l in name['words_result']])
                 else:
-                    s = ''.join([line[1][0] for res in result for line in res])
-                if "冰雪之歌" in s:
-                    ll[0].append(s)
-                if "夜行骑士" in s:
-                    ll[1].append(s)
-            except:
-                print('ocr fail!')
-                print(result)
-                continue
-        for n in range(2):
-            for s in ll[n]:
-                print('s0',s)
-                print('s0',s,file=ff)
-                # ff.write(str(id)+' '+s+'\n')
-
-                # 处理颜色代码bug
-
-                ket = ['1','l','J'] # 把括号识别成这些
+                    name = ''.join([l[1][0] for res in name for l in res])
+                # except:
+                #     print('ocr fail!')
+                #     print(name)
+                #     continue
+                if name[:2]=='单品':
+                    assert(dp==1)
+                    name = name[2:]
+                if name[:2]=='套品':
+                    dp = 0
+                    name = name[2:]
                 
-                for j in [6,5,4]:
-                    for i in range(len(s)-j): # 后j位一样
-                        w = s[i:i+j].lower()
-                        if sum(w==cl[-j:] for cl in cls):
-                            l,r = i,i+j
-                            if i and s[i-1] in ket:
-                                l -= 1
-                            if i+j<len(s) and (s[i+j]=='l' or s[i+j]=='J'):
-                                    r += 1
-                            if i+j<len(s) and s[i+j]=='1':
-                                if w[-4:] in ['6ab6','6a66','49e0']:
-                                        r += 1
-                                # if w=='da664f':
-                                #     if i+9<len(s) and s[i+6]=='1' and s[i+7]=='1' and : # 1121怎么办？去前还是去后？
-
-                                if w[-4:] == 'e96c':
-                                    if i+j+1<len(s) and (s[i+j+1]=='8' or s[i+j+1]=='9' or s[i+j+1]=='1'): # 黄色没有110+
-                                        r += 1
-                            s = s[:l]+' '+s[r:]
-                    for i in range(len(s)-j):  # 前j位一样
-                        w = s[i:i+j].lower()
-                        if sum(w==cl[:j] for cl in cls):
-                            l,r = i,i+j
-                            if i and s[i-1] in ket:
-                                l -= 1
-                            if i+j<len(s) and (s[i+j]=='l' or s[i+j]=='J'):
-                                    r += 1
-                            if i+j<len(s) and s[i+j]=='1':
-                                if w[1:4] in ['56a','949']:
-                                        r += 1
-                                # if w=='da664f':
-                                #     if i+9<len(s) and s[i+6]=='1' and s[i+7]=='1' and : # 1121怎么办？去前还是去后？
-
-                                if w[1:4] == 'fe9':
-                                    if i+j+1<len(s) and (s[i+j+1]=='8' or s[i+j+1]=='9' or s[i+j+1]=='1'): # 黄色没有110+
-                                        r += 1
-                            s = s[:l]+' '+s[r:]
-
-                # for i in range(len(s)-4):
-                #     if s[i:i+4]=='1000' or s[i:i+4]=='2023' or s[i:i+4]=='2022':
-                #         s = s[:i]+' '+s[i+4:]
+                print('s0',name)
+                print('s0',name,file=ff)
                 
                 m = {1:{'±':'+','：':':','（':'(','）':')','~':' ','|':' ','[':' ',']':' ','「':' ','－':'-','囊':'翼','［':' ','］':' ','」':' ','丨':' ','【':' ','】':' ','＋':'+','！':' ','，':' ','烊':'咩咩咩'},
                     2:{'萝下':'萝卜','一女':'-女','一男':'-男','型十':'型+','诸能':'储能','翘翔':'翱翔','翅翘':'翅','倒到':'倒','不到':'不倒','游夹':'游侠','铠电':'铠甲','骑土':'骑士','然擎':'燃擎','小五':'小丑','自镜':'目镜','钢电':'钢甲','魅景':'魅影','机电':'机甲','友型':'发型','萌免':'萌兔','手权':'手杖',
@@ -344,188 +309,58 @@ for id in range(id0,10000000):
                     3:{'迷夜翼':'迷璘夜翼','烊烊烊':'咩咩咩','流氓免':'流氓兔'}}
                     # 3:{}}
                 for size in range(3,0,-1):
-                    for i in range(len(s)-size+1):
-                        w = s[i:i+size]
+                    for i in range(len(name)-size+1):
+                        w = name[i:i+size]
                         if w in m[size]:
-                            s = s[:i]+m[size][w]+s[i+size:]
-                # for i in range(3,len(s)-1): # 处理 S**车王/神炫光 中S识别成5的问题
-                #     if s[i:i+2]=='车王' or s[i:i+2]=='车神':
-                #         if s[i-2]=='5':
-                #             s = s[:i-2]+' S'+s[i-1:]
-                #         elif s[i-3]=='5':
-                #             s = s[:i-3]+' S'+s[i-2:]
-                i = 0
-                while i<len(s):
-                    if s[i]=='S': # 分割 S**车王/神炫光
-                        s = s[:i]+' '+s[i:]
-                        i += 1
-                    i += 1
-                
-                print('s1',s)
-                print('s1',s,file=ff)
-                # ff.write(str(id)+' '+s+'\n')
+                            name = name[:i]+m[size][w]+name[i+size:]
+                print('s1',name)
+                print('s1',name,file=ff)
 
-                # l = [w for w in sum([re.split("[:+/. ]", ''.join(j)) for i,j in groupby(re.sub(u'\\[.*?\\]','',s), key=lambda x: x.isdecimal())],[]) if w]
-                l = [w for w in sum([re.split("[';:/·, ]|`", ''.join(j)) for i,j in groupby(s, key=lambda x: x.isdecimal() or x=='S')],[]) if w]
-                print('l',l)
-                print('l',l,file=ff)
-                o = 0 # 1 单品 2 套品
-                t = []
-                for w in l:
-                    if w[0]=='-':
-                        w = w[1:]
-                        if not w:
-                            continue
-                    if w=='单品':
-                        o = 1
-                        t = []
-                        continue
-                    elif w[:2]=='单品':
-                        o = 1
-                        t = [w[2:]]
-                        continue
-                    if w=='套品':
-                        o = 2
-                        t = []
-                        continue
-                    elif w[:2]=='套品':
-                        o = 2
-                        t = [w[2:]]
-                        continue
-                    elif w[1:3]=='套品':
-                        o = 2
-                        t = [w[3:]]
-                        continue
-                    if o==1: # 单品
-                        if w.isdecimal() and len(w)>1 and w not in ['1000','2021','2022','2023','10000','20520']: #一位数字加入文字中
-                            # 处理t
-                            if not t: # 防止 t[-1] 报错
-                                ff.write(f'\n??? {w} {id} {s}\n')
-                                continue
-                            ss = ''.join(t)
-                            # ss = ss.replace('^','','|','_')
-                            sp = '+'.join(t)
-                            while ss and ss[0] in ['-','l','品']:
-                                ss = ss[1:]
-                            if ss and ss[-1]=='开':
-                                ss = ss[:-1]
-                            ls = t[-1]
-                            t = []
-
-                            if int(w)>200:
-                                if w[0]=='1': #三位数
-                                    t.append(w[3:])
-                                    w = w[:3]
-                                else: #两位数
-                                    t.append(w[2:])
-                                    w = w[:2]
-                            w = int(w)
-                            
-                            print(f'dp{n} {ss},{w}')
-                            print(f'dp{n} {ss},{w}',file=ff)
-                            if ss in special or dp0[n]: # 新增未处理
-                                continue
-                            if '***' in ss: # 跑跑的bug
-                                continue
-                            # if ss in dlt and w==dlt[ss] and not test: # delete
-                            #     continue
-                            if ss in double[n] and w in double[n][ss]: # 两个值的
-                                continue
-                            if ss=='气球' and w in [91,39,31,36,41] or ss=='天气球' and w in [81,44] or ss=='头饰' and w==91:
-                                continue
-                            if ls in dp[n] and dp[n][ls]==w: # 最后一位有一样的不要了 防止前面有乱七八糟的东西
-                                continue
-                            if sp in tp and tp[n][sp]==w: # 套品有一样的也不要了
-                                continue
-                            if ss in dp[n] and dp[n][ss]==w: # 重复
-                                continue
-                            if ss in dp[n] and dp[n][ss]!=w: # 处理同物品不同value
-                                if not (w<20 or w>200 or w%100==dp[n][ss]): # 可能前面多个1
-                                    print(f'\nbyy!!! {ss} old:{dp[n][ss]} new:{w} 留old\n')
-                                    ff.write(f'\nbyy!!! {ss} old:{dp[n][ss]} new:{w} 留old\n')
-                                    byy = 1
-                                continue
-                            print('add?')
-                            print('add?',file=ff)
-                            if k==len(f)-1:
-                                dp[n][ss] = w
-                                # if ss not in dpp: 最后给用户看的时候再筛dpp 不然tuji中数据没发再发现了
-                                dp0[n][ss] = w
-                                add = 1
-                                print('add!')
-                                print('add!',file=ff)
-                            else:
-                                brk = 1
-                                break
-                        else:
-                            t.append(w)
-                    elif o==2: # 套品
-                        if w.isdecimal() and len(w)>1 and w not in ['1000','2021','2022','2023','10000','20520']: # 一位数字加入文字中
-                            if not t: # 防止 t[-1] 报错
-                                ff.write(f'\n??? {w} {id} {s}\n')
-                                continue
-                            if t[-1]=='l':
-                                t.pop()
-                            ss = ''.join(t)
-                            while ss and ss[0] in ['-','l','+']:
-                                ss = ss[1:]
-                            while ss and ss[-1] in ['开','+','.']:
-                                ss = ss[:-1]
-                            # ss = ss.replace('^','')
-                            t = []
-                            
-
-                            if int(w)>200:
-                                if w[0]=='1': #三位数
-                                    t.append(w[3:])
-                                    w = w[:3]
-                                else: #两位数
-                                    t.append(w[2:])
-                                    w = w[:2]
-
-                            # if len(t)<=1:
-                            #     print(id,'too short ',w,' ! ',s)
-                            w = int(w)
-                            
-                            print(f'tp{n} {ss},{w}')
-                            print(f'tp{n} {ss},{w}',file=ff)
-                            # if ss in dlt and w==dlt[ss] and not test: # delete
-                            #     continue
-                            if ss in tp0[n]: # 新增未处理
-                                continue
-                            if ss in tp[n] and tp[n][ss]==w: # 重复
-                                continue
-                            if ss in tp[n] and tp[n][ss]!=w: # 处理同物品不同value
-                                if not (w<20 or w>200 or w%100==tp[n][ss]): # 可能前面多个1
-                                    print(f'\nbyy!!! {ss} old:{tp[n][ss]} new:{w} 留old\n')
-                                    ff.write(f'\nbyy!!! {ss} old:{tp[n][ss]} new:{w} 留old\n')
-                                    byy = 1 # 可以考虑save和brk合并
-                                continue
-                            print('add?')
-                            print('add?',file=ff)
-                            if k==len(f)-1:
-                                tp[n][ss] = w
-                                tp0[n][ss] = w
-                                add = 1
-                                print('add!')
-                                print('add!',file=ff)
-                            else:
-                                brk = 1
-                                break
-                        else:
-                            t.append(w)
-                if brk:
-                    break
-            if brk:
-                break
-        # if brk:
-        #     image.save(f'img/{id}.png') # 调试 用test
-        #     print('brk')
-        if not brk: # 如果这个ocr函数break，就让下一个orc函数再试试
-            break
-            # with open('del_img.txt', 'a') as f:
-            #     f.seek(0,2)
-            #     f.write(str(i)+'\n')
+                if dp: # 单品的情况                         
+                    print(f'dp{n} {name},{num}')
+                    print(f'dp{n} {name},{num}',file=ff)
+                    if name in special or dp0[n]: # 新增未处理
+                        break
+                    if '***' in name: # 跑跑的bug
+                        break
+                    if name in double[n] and num in double[n][name]: # 两个值的
+                        break
+                    if name in dp[n] and dp[n][name]==num: # 重复
+                        break
+                    if name in dp[n] and dp[n][name]!=num: # 处理同物品不同value 不继续识别了 之后看怎么回事
+                        print(f'\nbyy!!! {name} old:{dp[n][name]} new:{num} 留old\n')
+                        ff.write(f'\nbyy!!! {name} old:{dp[n][name]} new:{num} 留old\n')
+                        byy = 1
+                        break
+                    print('add?')
+                    print('add?',file=ff)
+                    if k==len(f)-1:
+                        dp[n][name] = num
+                        # if name not in dpp: 最后给用户看的时候再筛dpp 不然tuji中数据没发再发现了
+                        dp0[n][name] = num
+                        add = 1
+                        print('add!')
+                        print('add!',file=ff)
+                else: # 套品   
+                    print(f'tp{n} {name},{num}')
+                    print(f'tp{n} {name},{num}',file=ff)
+                    if name in tp0[n]: # 新增未处理
+                        break
+                    if name in tp[n] and tp[n][name]==num: # 重复
+                        break
+                    if name in tp[n] and tp[n][name]!=num: # 处理同物品不同value
+                        print(f'\nbyy!!! {name} old:{tp[n][name]} new:{num} 留old\n')
+                        ff.write(f'\nbyy!!! {name} old:{tp[n][name]} new:{num} 留old\n')
+                        byy = 1
+                        break
+                    print('add?')
+                    print('add?',file=ff)
+                    if k==len(f)-1:
+                        tp[n][name] = num
+                        tp0[n][name] = num
+                        add = 1
+                        print('add!')
+                        print('add!',file=ff)
     if bd or byy: 
         # image.save(f'img/{id}.png')
         if not add:
@@ -545,6 +380,8 @@ for id in range(id0,10000000):
         os.remove(f"img/{id}.png")
     if not test:
         open('img/_ok.txt', 'w').write(str(id))
+    else:
+        break
 
     # print(dp,'\n',tp,'\n')
 
